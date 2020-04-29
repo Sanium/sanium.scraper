fc15912d7d27ff505833632ea68aca0a65c05a84
Bartłomiej Olszanowski <baloo@int.pl>
2020-04-29 20:14:32 +0200

    unified webdriver path

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sanium.scraper
+
+download [ChromeDrive](https://sites.google.com/a/chromium.org/chromedriver/home) and place it in `drivers` folder
--- main.py
+++ main.py
@@ -4,46 +4,51 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-option = webdriver.ChromeOptions()
-option.add_argument("-incognito")
 
-driver = webdriver.Chrome("C:\\Users\\ReQezeR\\Desktop\\chromedriver", options=option)
-driver.get("https://justjoin.it/")  # wczytanie podanej strony
+def main():
+    options = webdriver.ChromeOptions()
+    options.add_argument("-incognito")
+    options.add_argument('headless')
 
-root = driver.find_element_by_class_name('css-w6gvpm')#TODO: trzeba przesłać page down
-output = {}
+    driver = webdriver.Chrome("./drivers/chromedriver", options=options)
+    driver.get("https://justjoin.it/")  # wczytanie podanej strony
 
-key = 'o'
-while key != 'q':
-    lista = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[1]/div/div[2]')    # lista elementów znaleziona dzięki xpath
+    root = driver.find_element_by_class_name('css-w6gvpm')#TODO: trzeba przesłać page down
+    output = {}
 
-    elem = lista.find_elements_by_tag_name("a")     # ogłoszenia znalezione po tagu 'a'
-    # print("Wczytanych jest: ", elem.__len__(), " ogłoszeń \n==========")
+    key = 'o'
+    while key != 'q':
+        lista = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[1]/div/div[2]')    # lista elementów znaleziona dzięki xpath
 
-    # przeszukujemy ogłoszenie w poszukiwaniu konkretnych elementów
-    for x in elem:
-        output[x.get_attribute("href").__str__().replace('https://justjoin.it/offers/', '')] = {
-            'title': x.find_element_by_class_name('css-wjfk7i').text,
-            'info': x.find_element_by_class_name('css-jbk0sa').text,
-            'salary': x.find_element_by_class_name('css-1dotj4s').text,
-            'status': x.find_element_by_class_name('css-hw5uoy').text
-        }
-        # print("Slug ID: ", x.get_attribute("href").__str__().replace('https://justjoin.it/offers/', ''), "\n"
-        #       "Tytuł:", x.find_element_by_class_name('css-wjfk7i').text, "\n",  #element znaleziony po nazwie klasy
-        #       "Info:", x.find_element_by_class_name('css-jbk0sa').text, "\n", #element znaleziony po nazwie klasy
-        #       "Płaca:", x.find_element_by_class_name('css-1dotj4s').text, "\n",#element znaleziony po nazwie klasy
-        #       "Status:", x.find_element_by_class_name('css-hw5uoy').text, "\n==========")#element znaleziony po nazwie klasy
+        elem = lista.find_elements_by_tag_name("a")     # ogłoszenia znalezione po tagu 'a'
+        # print("Wczytanych jest: ", elem.__len__(), " ogłoszeń \n==========")
 
-    # root.send_keys(Keys.PAGE_DOWN)#TODO: wywołanie scrollowania
-    # driver.implicitly_wait(5)
-    key = input("Press q to quit: ")
+        # przeszukujemy ogłoszenie w poszukiwaniu konkretnych elementów
+        for x in elem:
+            output[x.get_attribute("href").__str__().replace('https://justjoin.it/offers/', '')] = {
+                'title': x.find_element_by_class_name('css-wjfk7i').text,
+                'info': x.find_element_by_class_name('css-jbk0sa').text,
+                'salary': x.find_element_by_class_name('css-1dotj4s').text,
+                'status': x.find_element_by_class_name('css-hw5uoy').text
+            }
+            # print("Slug ID: ", x.get_attribute("href").__str__().replace('https://justjoin.it/offers/', ''), "\n"
+            #       "Tytuł:", x.find_element_by_class_name('css-wjfk7i').text, "\n",  #element znaleziony po nazwie klasy
+            #       "Info:", x.find_element_by_class_name('css-jbk0sa').text, "\n", #element znaleziony po nazwie klasy
+            #       "Płaca:", x.find_element_by_class_name('css-1dotj4s').text, "\n",#element znaleziony po nazwie klasy
+            #       "Status:", x.find_element_by_class_name('css-hw5uoy').text, "\n==========")#element znaleziony po nazwie klasy
 
-json = json.dumps(output)
-f = open("dict.json", "w")
-f.write(json)
-f.close()
+        # root.send_keys(Keys.PAGE_DOWN)#TODO: wywołanie scrollowania
+        # driver.implicitly_wait(5)
+        key = input("Press q to quit: ")
 
-time.sleep(3)
-driver.close()
+    json_data = json.dumps(output)
+    f = open("dict.json", "w")
+    f.write(json_data)
+    f.close()
+
+    time.sleep(3)
+    driver.close()
 
 
+if __name__ == "__main__":
+    main()
