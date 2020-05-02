9753ea06fb28a7e565b426eac12f7e6f24822223
Bartłomiej Olszanowski <baloo@int.pl>
2020-05-02 12:09:08 +0200

    Project's scaffold

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask
+from app.models import db
+
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sanium.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+)
+
+db.init_app(app)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+with app.app_context():
+    from . import models, controllers, services
+
+    db.create_all()
+    models.init_app(app)
+    controllers.init_app(app)
+    services.init_app(app)
