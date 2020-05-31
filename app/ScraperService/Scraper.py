a8fee6b35d2a4cd006a21255a12f388cf8c80403
ReQezeR <mp62639@o2.pl>
2020-05-31 18:38:52 +0200

    update ScraperService

--- app/ScraperService/Scraper.py
+++ app/ScraperService/Scraper.py
@@ -19,48 +19,60 @@ def safe_find_elem(parent, locator):
 
 
 class Scraper:
-    def __init__(self):
-        print("Init [", datetime.now(), ']')
+    def __init__(self, website, debug=False):
         self.output = {}
+        self.debug = debug
+        self.website = website
         self.options = webdriver.ChromeOptions()
         self.options.add_argument("-incognito")
         self.options.add_argument('headless')
         self.driver = webdriver.Chrome("./drivers/chromedriver", options=self.options)
+        if self.debug: print("Init [", datetime.now(), ']')
 
     def __del__(self):
-        print("Fin [", datetime.now(), ']')
+        if self.debug: print("Fin [", datetime.now(), ']')
         self.driver.close()
 
-    def run_main_page_scrapping(self, url, target_number):
+    def get_data(self):
+        return self.output
+
+    def set_data(self, data):
+        self.output = data
+
+    def run_main_page_scrapping(self, target_number):
         # driver.get(url)  # wczytanie podanej strony
-        if url == "https://justjoin.it/":
+        if self.website == "https://justjoin.it/":
             main_page = Justjoin_MainPage.MainPage(self.driver)
-            print("Main Page [START : ", datetime.now(), ']')
+            if self.debug: print("Main Page [START : ", datetime.now(), ']')
             self.output = main_page.get_n_offers_from_list(target_number)
-            print("Main Page [END : ", datetime.now(), ']')
-            print("Number of elements: ", self.output.__len__())
+            if self.debug: print("Main Page [END : ", datetime.now(), ']')
+            if self.debug: print("Number of elements: ", self.output.__len__())
             # print(list(self.output.keys())[0])
         return self.output
 
-    def run_detail_page_scrapping(self):
-        print("Detail Page [START : ", datetime.now(), ']')
-        for i in range(self.output.__len__()):
-            detail_page = Justjoin_DetailPage.DetailPage(self.driver, list(self.output.keys())[i])
-            detail_data = detail_page.get_data()
-            self.output[list(self.output.keys())[i]]['location'] = detail_data[list(self.output.keys())[i]]['location']
-            self.output[list(self.output.keys())[i]]['employer'] = detail_data[list(self.output.keys())[i]]['employer']
-            self.output[list(self.output.keys())[i]]['experience'] = detail_data[list(self.output.keys())[i]]
-            ['experience']
-            self.output[list(self.output.keys())[i]]['employment'] = detail_data[list(self.output.keys())[i]]
-            ['employment']
-            self.output[list(self.output.keys())[i]]['description'] = detail_data[list(self.output.keys())[i]]
-            ['description']
-        print("Detail Page [END : ", datetime.now(), ']')
+    def run_detail_page_scrapping(self, target_id):
+        if self.debug: print("Detail Page [START : ", datetime.now(), ']')
+        detail_page = Justjoin_DetailPage.DetailPage(self.driver, target_id, debug=self.debug)
+        detail_data = detail_page.get_data()
+        self.output[target_id] = {}
+
+        self.output[target_id]['title'] = detail_data[target_id]['title']
+        self.output[target_id]['salary'] = detail_data[target_id]['salary']
+        self.output[target_id]['salary_from'] = detail_data[target_id]['salary_from']
+        self.output[target_id]['salary_to'] = detail_data[target_id]['salary_to']
+        self.output[target_id]['currency'] = detail_data[target_id]['currency']
+        self.output[target_id]['street'] = detail_data[target_id]['street']
+        self.output[target_id]['city'] = detail_data[target_id]['city']
+        self.output[target_id]['employer'] = detail_data[target_id]['employer']
+        self.output[target_id]['experience'] = detail_data[target_id]['experience']
+        self.output[target_id]['employment'] = detail_data[target_id]['employment']
+        self.output[target_id]['description'] = detail_data[target_id]['description']
+        if self.debug: print("Detail Page [END : ", datetime.now(), ']')
 
     def save_data(self):
-        print("Save [START : ", datetime.now(), ']')
+        if self.debug: print("Save [START : ", datetime.now(), ']')
         json_data = json.dumps(self.output)
         f = open("../../dict.json", "w")
         f.write(json_data)
         f.close()
-        print("Save [END : ", datetime.now(), ']')
+        if self.debug: print("Save [END : ", datetime.now(), ']')
--- app/ScraperService/__init__.py
+++ app/ScraperService/__init__.py
@@ -1,4 +1,6 @@
 from time import sleep
+
+from app.ScraperService.Scraper import Scraper
 from app.models.Job import Job
 from app.services.SchedulerService import create_job
 
@@ -6,9 +8,23 @@ from app.services.SchedulerService import create_job
 def main_page_job(*args, **kwargs):
     """
     Job function of scraping main page. It will by executed if user schedule scraping.
+    args = [job_id, website, offers_count_number]
     """
-    # TODO scrape main page
-    # TODO save all found links
+    # get args
+    website = args[1]
+    offers_count_number = args[2]
+
+    # init scraper
+    scraper = Scraper(website)
+
+    # run scrapping on main page
+    scraper.run_main_page_scrapping(offers_count_number)
+
+    # get basic offer data
+    data = scraper.get_data()
+
+    scraper.save_data()
+
     # TODO schedule scraping for each link
 
     sleep(1)
@@ -18,21 +34,30 @@ def main_page_job(*args, **kwargs):
     j: Job = Job.find(job_id)
     print(j.name, j.run_date)
     sleep(1)
-    detail: Job = create_job('detail_page_job', detail_page_job, args=['conversion-it-project-manager-ecommerce'], seconds=15)
-    print('detail run: ', detail.run_date)
+    # TODO check
+    for i in range(scraper.output.__len__()):
+        offer_id = list(data.keys())[i]
+        detail: Job = create_job('detail_page_job', detail_page_job, args=[i, "https://justjoin.it/", offer_id], seconds=15)
+        print('detail run: ', detail.run_date)
 
 
 def detail_page_job(*args, **kwargs):
     """
     Job function of scraping specified page with details about offer. It will be scheduled by main_page_job
+    args = [job_id, website, offer_id]
     """
-    # TODO scrape detail page
+    website = args[1]
+    offer_id = args[2]
+    scraper = Scraper(website)
+
+    scraper.run_detail_page_scrapping(offer_id)
+    scraper.save_data()  # save to file
+    scraper.get_data()  # get data as dict
     # TODO save result in db as Offer
 
     job_id = args[0]
     j: Job = Job.find(job_id)
     print(j.name, j.run_date)
     sleep(1)
-    site_id = args[1]
-    print('site id:', site_id)
+    print('site id:', website)
     sleep(1)
