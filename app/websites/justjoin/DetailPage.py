fd5ace70dc4584af324d1fa88e6f73d2bcefb297
ReQezeR <mp62639@o2.pl>
2020-05-31 18:36:37 +0200

    regex salary and location

--- app/websites/justjoin/DetailPage.py
+++ app/websites/justjoin/DetailPage.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+import re
 
 
 class DetailPageLocators:
@@ -14,18 +15,49 @@ class DetailPageLocators:
     OFFER_LOCATION = (By.CLASS_NAME, 'css-1d6wmgf')
     COMPANY_LOGO = (By.CLASS_NAME, 'css-17jro83')
     COMPANY_NAME = (By.CLASS_NAME, 'css-l4opor')
-    #TODO: inny typ lokalizacji dla exp i emp
+    # TODO: inny typ lokalizacji dla exp i emp
     OFFER_EXPERIENCE = (By.CLASS_NAME, 'css-1ji7bvd')
     OFFER_EMPLOYMENT = (By.CLASS_NAME, 'css-1ji7bvd')
     OFFER_DESCRIPTION = (By.CLASS_NAME, 'css-u2qsbz')
 
 
 class DetailPage:
-    def __init__(self, driver, key):
+    def __init__(self, driver, key, debug=False):
         self.driver = driver
         self.key = key
         self.url = "https://justjoin.it/offers/" + self.key
         self.driver.get(self.url)
+        self.debug = debug
+
+    def format_salary(self, salary):
+        if type(salary) is tuple:
+            salary = salary[0]
+        from_to = re.findall(r'([\d ]+)', salary)
+        currency = re.findall(r'([A-Z]+)', salary)
+        if len(from_to) >= 2:
+            salary_from = from_to[0].replace(' ', '')
+            salary_to = from_to[1].replace(' ', '')
+            if len(currency) >= 1 and len(currency[0]) == 3:
+                currency = currency[0]
+                salary = '%s %s %s' % (salary_from, salary_to, currency)
+                return {'salary': salary, 'salary_from': salary_from, 'salary_to': salary_to, 'currency': currency}
+            else:
+                salary = '%s %s' % (salary_from, salary_to)
+                return {'salary': salary, 'salary_from': salary_from, 'salary_to': salary_to, 'currency': None}
+        else:
+            return {'salary': salary.strip(), 'salary_from': None, 'salary_to': None, 'currency': None}
+
+    def format_location(self, location):
+        if type(location) is tuple:
+            location = location[0]
+
+        location = location.split(',')
+        if len(location) >= 2:
+            city = location[1]
+            street = location[0]
+            return {'city': city, 'street': street}
+        else:
+            return {'city': None, 'street': None}
 
     def get_data(self):
         def find(parent, locator):
@@ -47,7 +79,7 @@ class DetailPage:
                 print(e)
 
         output = {}  # dane wyjściowe
-        print("Wczytanie oferty [START : ", datetime.now(), ']')
+        if self.debug: print("Wczytanie oferty [START : ", datetime.now(), ']')
         WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(DetailPageLocators.OFFER_TITLE))
 
         title = self.driver.find_element(*DetailPageLocators.OFFER_TITLE).text
@@ -55,15 +87,25 @@ class DetailPage:
         company_size = basic[0][0]
         employment = basic[0][1]
         experience = basic[0][2]
+
+        salary = find(self.driver, DetailPageLocators.OFFER_SALARY)
+        salary = self.format_salary(salary)
+
+        location = find(self.driver, DetailPageLocators.OFFER_LOCATION)
+        location = self.format_location(location)
+
         output[self.key] = {
             'title': title,
-            'salary': find(self.driver, DetailPageLocators.OFFER_SALARY),
-            'location': find(self.driver, DetailPageLocators.OFFER_LOCATION),
+            'salary': salary['salary'],
+            'salary_from': salary['salary_from'],
+            'salary_to': salary['salary_to'],
+            'currency': salary['currency'],
+            'street': location['street'],
+            'city': location['city'],
             'employer': find(self.driver, DetailPageLocators.COMPANY_NAME),
             'experience': experience,
             'employment': employment,
             'description': find(self.driver, DetailPageLocators.OFFER_DESCRIPTION)
-
         }
-        print("Wczytanie oferty [END : ", datetime.now(), ']')
+        if self.debug: print("Wczytanie oferty [END : ", datetime.now(), ']')
         return output
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from app.ScraperService.Scraper import Scraper
 
-
 def main():
-    scraper = Scraper()
-    scraper.run_main_page_scrapping("https://justjoin.it/", 5)
-    scraper.run_detail_page_scrapping()
-    scraper.save_data()
+    scraper = Scraper("https://justjoin.it/")
+    scraper.run_main_page_scrapping(5)
+
+    s2 = Scraper("https://justjoin.it/")
+    s2.run_detail_page_scrapping(list(scraper.get_data().keys())[0])
+    s2.run_detail_page_scrapping(list(scraper.get_data().keys())[1])
+    s2.run_detail_page_scrapping(list(scraper.get_data().keys())[2])
+    s2.save_data()
 
 
 if __name__ == "__main__":
